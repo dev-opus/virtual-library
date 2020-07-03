c8ddd4c441f959bb801779c84257345ff3c19e3c
dev-opus <vicorlunda@gmail.com>
2020-07-03 11:12:35 +0100

    create rendering functionality of the app

--- js/main.js
+++ js/main.js
@@ -18,10 +18,17 @@ closeButton.addEventListener('click', () => openAndClose(modalConatianer));
     THE BOOK ARRAY, BOOK CONSTRUCTOR AND ADDBOOK FUNCTION ALL LIVE HERE!
 */
 
-let bookTitle = document.querySelector('#title');
-let bookAuthour = document.querySelector('#authour');
-let bookPages = document.querySelector('#pages');
-let bookStatus = document.querySelector('#read');
+const titleContainer = document.getElementById('title');
+const authourContainer = document.getElementById('authour');
+const pagesContainer = document.getElementById('pages');
+const statusContainer = document.getElementById('read');
+const bookHolder = document.querySelector('.book-holder');
+
+let bookTitle;
+let bookAuthour;
+let bookPages;
+let bookStatus;
+let bookIndex;
 
 const bookArray = [];
 
@@ -37,3 +44,92 @@ function addBookToArray() {
 	bookArray.push(newBook);
 }
 
+/* 
+	STAGE AND DISPLAY A BOOK TO THE ARRAY SECTION
+*/
+
+const renderButton = document.querySelector('.form-add');
+
+function stageBook() {
+	bookTitle = titleContainer.value;
+	bookAuthour = authourContainer.value;
+	bookPages = pagesContainer.value;
+	bookStatus = statusContainer.checked;
+
+	titleContainer.value = '';
+	authourContainer.value = '';
+	pagesContainer.value = '';
+
+	if (statusContainer.checked) statusContainer.checked = false;
+}
+
+function displayBook() {
+	bookIndex = bookArray.length - 1;
+
+	const bookConatiner = document.createElement('div');
+	bookConatiner.classList.toggle('book-container');
+	bookConatiner.setAttribute('data-index', bookIndex);
+
+	const bookTitle = document.createElement('div');
+	bookTitle.classList.toggle('first');
+	bookTitle.textContent = bookArray[bookIndex].title;
+	bookConatiner.appendChild(bookTitle);
+
+	const bookAuthour = document.createElement('div');
+	bookAuthour.classList.toggle('second');
+	bookAuthour.textContent = bookArray[bookIndex].authour;
+	bookConatiner.appendChild(bookAuthour);
+
+	const bookPages = document.createElement('div');
+	bookPages.classList.toggle('third');
+	bookPages.textContent = bookArray[bookIndex].pages;
+	bookConatiner.appendChild(bookPages);
+
+	const bookStatus = document.createElement('div');
+	bookStatus.classList.toggle('fourth');
+	bookStatus.textContent = 'read';
+
+	if (bookArray[bookIndex].readStatus) {
+		bookStatus.classList.toggle('read');
+	} else {
+		bookStatus.classList.toggle('not-read');
+	}
+
+	bookStatus.addEventListener('click', () => {
+		if (bookStatus.classList.contains('read')) {
+			return bookStatus.classList.toggle('not-read');
+		}
+		if (bookStatus.classList.contains('not-read')) {
+			return bookStatus.classList.toggle('read')
+		}
+	});
+
+	if (bookTitle.textContent === '' || bookPages.textContent === '' || bookAuthour.textContent === '') {
+		alert('Invalid entry!!')
+		return
+	}
+	bookConatiner.appendChild(bookStatus);
+
+	const deleteContainer = document.createElement('div');
+	deleteContainer.classList.toggle('fifth');
+
+	const deleteIcon = document.createElement('img');
+	deleteIcon.setAttribute('src', 'img/delete.svg');
+
+	deleteIcon.classList.toggle('delete');
+	deleteIcon.addEventListener('click', () => {
+		bookArray.splice(bookIndex, 1);
+		bookConatiner.remove();
+	});
+
+	deleteContainer.appendChild(deleteIcon);
+	bookConatiner.appendChild(deleteContainer);
+
+	bookHolder.appendChild(bookConatiner);
+}
+
+renderButton.addEventListener('click', () => {
+	stageBook();
+	addBookToArray();
+	displayBook();
+});
