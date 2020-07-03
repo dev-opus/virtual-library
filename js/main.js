3c0b12f5a4782a9c13248e2b199680d6e83786e5
dev-opus <vicorlunda@gmail.com>
2020-07-03 02:11:31 +0100

    create the app engine - bookarray, book constructor and addbook func

--- js/main.js
+++ js/main.js
@@ -13,3 +13,27 @@ function openAndClose(param) {
 openbutton.addEventListener('click', () => openAndClose(modalConatianer));
 
 closeButton.addEventListener('click', () => openAndClose(modalConatianer));
+
+/*
+    THE BOOK ARRAY, BOOK CONSTRUCTOR AND ADDBOOK FUNCTION ALL LIVE HERE!
+*/
+
+let bookTitle = document.querySelector('#title');
+let bookAuthour = document.querySelector('#authour');
+let bookPages = document.querySelector('#pages');
+let bookStatus = document.querySelector('#read');
+
+const bookArray = [];
+
+function Book(title, authour, pages, readStatus) {
+	this.title = title;
+	this.authour = authour;
+	this.pages = pages;
+	this.readStatus = readStatus;
+}
+
+function addBookToArray() {
+	const newBook = new Book(bookTitle, bookAuthour, bookPages, bookStatus);
+	bookArray.push(newBook);
+}
+
